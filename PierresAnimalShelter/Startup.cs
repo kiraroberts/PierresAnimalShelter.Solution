722b569ff88c4205a3b233ae05112a202d992ebc
Kira Roberts <rosehill1788@protonmail.com>
2019-11-01 12:39:00 -0700

    fix typo in startup.cs, removed unnecessary pierresanimalshelter.models using directive

--- PierresAnimalShelter/Startup.cs
+++ PierresAnimalShelter/Startup.cs
@@ -1,5 +1,4 @@
-﻿using PierresAnimalShelter.Models;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
