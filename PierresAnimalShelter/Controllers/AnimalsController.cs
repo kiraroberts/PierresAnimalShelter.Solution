2bc50b46b06265517bef9b5e191571e8b02adc1d
Kira Roberts <rosehill1788@protonmail.com>
2019-11-01 14:43:24 -0700

    add AnimalsController.cs

---
+++ PierresAnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PierresAnimalShelter.Models;
+
+namespace PierresAnimalShelter.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private PierresAnimalShelterContext _db;
+        public AnimalsController(PierresAnimalShelterContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        public void Post([FromBody] Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+        }
+    }
+}
