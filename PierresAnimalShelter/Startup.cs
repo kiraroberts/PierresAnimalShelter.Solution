078fd5d4c439cc3fb508906e0abdde7f8ed57e73
Kira Roberts <rosehill1788@protonmail.com>
2019-11-01 12:07:09 -0700

    fix typo in startup.cs

--- PierresAnimalShelter/Startup.cs
+++ PierresAnimalShelter/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
