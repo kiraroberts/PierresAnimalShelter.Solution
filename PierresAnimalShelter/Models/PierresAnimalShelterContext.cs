e34364aa1bc9e205c788df50b389760e305360e4
Kira Roberts <rosehill1788@protonmail.com>
2019-11-01 16:46:08 -0700

    add override method to PierresAnimalSheltercontext.cs

--- PierresAnimalShelter/Models/PierresAnimalShelterContext.cs
+++ PierresAnimalShelter/Models/PierresAnimalShelterContext.cs
@@ -12,4 +12,14 @@ namespace PierresAnimalShelter
             }
         public DbSet<Animal> Animals { get; set; }
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Animal>()
+            .HasData(
+                new Animal { AnimalId = 1, Name = "Tony", Species = "Cat", Age = 3, Gender = "Male", Description = "An orange tabby." },
+                new Animal { AnimalId = 2, Name = "Gretchen", Species = "Dog", Age = 10, Gender = "Female", Description = "A gray terrier mix." },
+                new Animal { AnimalId = 3, Name = "Nacho", Species = "Cat", Age = 4, Gender = "Male", Description = "An orange tabby." },
+                new Animal { AnimalId = 4, Name = "Urusula", Species = "Mouse", Age = 1, Gender = "Female", Description = "A white mouse." },
+            );
+    }
 }
