506840e106280659f2da22b7495f01ecd45816d2
Kira Roberts <rosehill1788@protonmail.com>
2019-11-17 21:48:11 -0800

    remove ValuesController.cs

---
+++ PierresAnimalShelter/Migrations/20191107073746_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresAnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Description", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 3, "An orange tabby.", "Male", "Tony", "Cat" },
+                    { 2, 10, "A gray terrier mix.", "Female", "Gretchen", "Dog" },
+                    { 3, 4, "An orange tabby.", "Male", "Nacho", "Cat" },
+                    { 4, 1, "A white mouse.", "Female", "Urusula", "Mouse" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+        }
+    }
+}
